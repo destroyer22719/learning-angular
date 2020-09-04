f7839772dbf7db1c4f5f47a476304f64bf224577
Nathan <nathancai1031@gmail.com>
2020-09-04 14:09:40 -0400

    done

--- src/app/components/new-user-reactive/new-user-reactive.component.ts
+++ src/app/components/new-user-reactive/new-user-reactive.component.ts
@@ -14,25 +14,25 @@ export class NewUserReactiveComponent implements OnInit {
 
   ngOnInit(): void {
     this.userForm = new FormGroup({
-      name:new FormControl(''),
-      username:new FormControl(''),
-      email:new FormControl(''),
-      phone:new FormControl(''),
-      website:new FormControl(''),
-      address:new FormControl({
-        street:new FormControl(''),
-        suite:new FormControl(''),
-        city:new FormControl(''),
-        zipcode:new FormControl(''),
-        geo:new FormControl({
-          lat:new FormControl(''),
-          lng:new FormControl(''),
+      name:new FormControl('John Doe'),
+      username:new FormControl('johndoe'),
+      email:new FormControl('john.doe@example.com'),
+      phone:new FormControl('1234567890'),
+      website:new FormControl('john.com'),
+      address:new FormGroup({
+        street:new FormControl('123 Street St.'),
+        suite:new FormControl('Suite 123'),
+        city:new FormControl('City'),
+        zipcode:new FormControl('123456'),
+        geo:new FormGroup({
+          lat:new FormControl('123.12314'),
+          lng:new FormControl('123.123213'),
         }),
       }),
-      company:new FormControl({
-        name:new FormControl(''),
-        catchPhrase:new FormControl(''),
-        bs:new FormControl(''),
+      company:new FormGroup({
+        name:new FormControl('My Company'),
+        catchPhrase:new FormControl('my catchphrase'),
+        bs:new FormControl('What the heck is a bs anyways?'),
       }),
     })
   }
