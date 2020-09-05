3d9176498d38a9f58f7c9f5526f65761493b374d
Nathan <nathancai1031@gmail.com>
2020-09-05 13:27:40 -0400

    done

--- src/app/classes/custom.validations.ts
+++ src/app/classes/custom.validations.ts
@@ -4,4 +4,12 @@ export class CustomValidation {
     static unique(control:AbstractControl):ValidationErrors | null {
         return control.value === 'kold' ? {unique:true}:null;
     }
+    static asyncUnique(control:AbstractControl): Promise<ValidationErrors | null>| null {
+        const myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                control.value === 'kold' ? resolve({asyncUnique:true}): resolve(null);
+            }, 1000)
+        })
+        return myPromise
+     }
 }
